595d81055371ada6996a853de34fc186f0ff9a43
Nilesh Gadhe <nilesh.gadhe@innofied.com>
2021-06-01 18:23:44 +0530

    no message

--- ios/InLocalCustomer/InLocalCustomer/Common/CustomViews/ValidateOTP.swift
+++ ios/InLocalCustomer/InLocalCustomer/Common/CustomViews/ValidateOTP.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import OTPFieldView
 
 class ValidateOTP: UIView {
 
     @IBOutlet weak var viewBckGrnd: UIView!
     @IBOutlet weak var imgViewBckGrnd: UIImageView!
+    @IBOutlet var otpTextFieldView: OTPFieldView!
+    
     
     class func instanceFromNib() -> ValidateOTP{
-        return UINib(nibName: "ValidateOTP", bundle: nil).instantiate(withOwner: self, options: nil)[0] as! ValidateOTP
+        let view = UINib(nibName: "ValidateOTP", bundle: nil).instantiate(withOwner: self, options: nil)[0] as! ValidateOTP
+        view.setupOtpView()
+        return view
+    }
+
+    func setupOtpView(){
+        self.otpTextFieldView.fieldsCount = 6
+        self.otpTextFieldView.fieldBorderWidth = 2
+        self.otpTextFieldView.defaultBorderColor = UIColor.lightGray
+        self.otpTextFieldView.filledBorderColor = UIColor.darkGray
+        self.otpTextFieldView.cursorColor = UIColor.black
+        self.otpTextFieldView.displayType = .square
+        self.otpTextFieldView.fieldSize = 50
+        self.otpTextFieldView.separatorSpace = 10
+        self.otpTextFieldView.shouldAllowIntermediateEditing = false
+            //self.otpTextFieldView.delegate = self
+        self.otpTextFieldView.initializeUI()
     }
 
 }
